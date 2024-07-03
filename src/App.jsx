4301d8849f6dc4ff79038fbfde71c69a71cd2ebe
Prabhudatta19 <prabhudatta.prof@gmail.com>
2024-07-04 01:32:55 +0530

    navbar initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavBar from './Components/NavBar'
 import SearchBar from './Components/SearchBar'
 import logo from '/world.svg'
 
 function App() {
   return (
     <>
+      <NavBar/>
       <div>
         <a href="/">
           <img src={logo} className="logo react" alt="React logo" />
